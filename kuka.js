0de1c82d9bbbd6118776ab7c813eec30de729715
Isaac Toleafoa <isaactoleafoa@gmail.com>
2016-11-02 17:13:45 -0600

    modal project files

---
+++ kuka.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById('mymodal');
+var close = document.getElementsByTagName('span')[0];
+
+function appear() {
+	modal.style.display = 'block';
+}
+// Will wait 3 seconds before appearing
+setTimeout(appear, 3000);
+
+// close button to close modal
+close.onclick = function() {
+	modal.style.display = "none";
+}
+
